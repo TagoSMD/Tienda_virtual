fee572a08bfe414f3f404b3ff28f89be310a3d0d
TagoSMD <jacobgt890@gmail.com>
2023-06-07 20:28:38 -0600

    agregando mvc y librerias

--- index.php
+++ index.php
@@ -1,57 +1,32 @@
-<?php
-      require_once("Config/Config.php");
-         $url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
-          $arrUrl = explode("/", $url);
-          $controller = $arrUrl[0];
-          $method = $arrUrl[0];
-          $params = "";
+<?php 
+	require_once("Config/Config.php");
+	require_once("Helpers/Helpers.php");
+	$url = !empty($_GET['url']) ? $_GET['url'] : 'home/home';
+	$arrUrl = explode("/", $url);
+	$controller = $arrUrl[0];
+	$method = $arrUrl[0];
+	$params = "";
 
-          if(!empty($arrUrl[1]))
-          {
-          	if($arrUrl[1] != "")
-          	{
-          		$method = $arrUrl[1];
-          	}
-          }
+	if(!empty($arrUrl[1]))
+	{
+		if($arrUrl[1] != "")
+		{
+			$method = $arrUrl[1];	
+		}
+	}
 
-          if(!empty($arrUrl[2]))
-          {
-          	if($arrUrl[2] != "")
-          	{
-          		for($i=2; $i < count($arrUrl); $i++){
-          			$params .= $arrUrl[$i].',';
+	if(!empty($arrUrl[2]))
+	{
+		if($arrUrl[2] != "")
+		{
+			for ($i=2; $i < count($arrUrl); $i++) {
+				$params .=  $arrUrl[$i].',';
+				# code...
+			}
+			$params = trim($params,',');
+		}
+	}
+	require_once("Libraries/Core/Autoload.php");
+	require_once("Libraries/Core/Load.php");
 
-          		}
-          		$params = trim($params,',');
-          		   }
-          }
-
-       spl_autoload_register(function($class){
-
-       	if(file_exists(LIBS. 'Core/'.$class.".php")){
-       		require_once(LIBS.'Core/'.$class. ".php");
-       	}
-       }); 
-
-
-       //load
-       $controllerFile = "Controllers/" .$controller. ".php";
-       if(file_exists($controllerFile))
-       {
-
-       	require_once($controllerFile);
-       	$controller = new $controller();
-
-       }else{
-       	echo "no Existe controlador";
-       }
-
-
-echo "<br>";
-echo "controlador: ".$controller;
-echo "<br";
-echo "método:".$method;
-echo "<br>";
-echo "parametros:".$params;
-                   
-    ?>
+ ?>
